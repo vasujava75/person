0e8c3690cad8eb98e807d64b3ef74b7151191ee1
vasudev <vasudev.kondepati@kpn.com>
2019-11-03 13:40:14 +0100

    removed unwanted changes

--- person-respository/src/main/java/com/rebo/person/respository/PersonRepository.java
+++ person-respository/src/main/java/com/rebo/person/respository/PersonRepository.java
@@ -1,14 +1,14 @@
 package com.rebo.person.respository;
 
 import com.rebo.person.domain.Person;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 
-@RepositoryRestResource(collectionResourceRel = "people", path = "people")
-public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
     List<Person> findByLastName(@Param("name") String name);
 }
